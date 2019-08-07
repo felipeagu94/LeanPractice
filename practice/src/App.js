7e2b518216c45c2a1981ae073e1a6e61c4a208b5
Juan Felipe Agudelo Zapata <felipe-agu@hotmail.com>
2019-08-07 18:24:13 -0500

    Base

---
+++ practice/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './components/menu'
+
+function App() {
+  return (
+    <div>
+      <Menu></Menu>
+      <h1>Welcome!</h1>
+    </div>
+  );
+}
+
+export default App;
